24a785436493a7f8a0c424785977be1454c01800
Phu Sang <sang.le@neopay.vn>
2022-06-30 16:47:59 +0700

    up

---
+++ ngrok.js
@@ -0,0 +1,24 @@
+const ngrok = require('ngrok');
+
+const connection = async () => {
+    const url = await ngrok.connect({
+        proto: 'http', // http|tcp|tls, defaults to http
+        addr: 8080, // port or network address, defaults to 80
+        // auth: 'user:pwd', // http basic authentication for tunnel
+        // subdomain: 'alex', // reserved tunnel name https://alex.ngrok.io
+        authtoken: '2BHPntMU2XV54mlMGYugcrWLEYV_6YRykQfBWYwMsKUUhvzr7', // your authtoken from ngrok.com
+        region: 'us', // one of ngrok regions (us, eu, au, ap, sa, jp, in), defaults to us
+        // configPath: '~/git/project/ngrok.yml', // custom path for ngrok config file
+        // binPath: path => path.replace('app.asar', 'app.asar.unpacked'), // custom binary path, eg for prod in electron
+        onStatusChange: status => {}, // 'closed' - connection is lost, 'connected' - reconnected
+        onLogEvent: data => {}, // returns stdout messages from ngrok process
+    });
+    
+    console.log('URL',url);
+    
+}
+
+console.log('Dang');
+
+
+connection();
---
+++ webhook.js
@@ -0,0 +1,38 @@
+// init project
+var express = require("express");
+var path = require("path");
+// Data is stored in the file `database.json` in the folder `db`.
+// Note that if you leave your app public, this database file will be copied if
+// someone forks your app. So don't use it to store sensitive information.
+var app = express();
+var bodyParser = require("body-parser");
+const srcPath = __dirname;
+// Using `public` for static files: http://expressjs.com/en/starter/static-files.html
+app.use(express.static(path.join(srcPath, "public")));
+// Use bodyParser to parse application/x-www-form-urlencoded form data
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+// Create a new entry in the users table
+app.post("/webhook", urlencodedParser, function(request, response) {
+ console.log("REQUEST" ,request);
+ console.log("RESPONSE" ,response);
+ 
+});
+
+const bot =require("./telegram-bot")
+
+// Serve the root url: http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function(request, response) {
+  response.sendFile(path.join(srcPath, "views", "index.html"));
+});
+
+app.post("/webhook", urlencodedParser, function(request, response) {
+  console.log("REQUEST" ,request);
+  console.log("RESPONSE", response);
+  bot.telegrambot(request)
+  
+ });
+// Listen on port 8080
+var listener = app.listen(8080, function() {
+  console.log("Listening on port " + listener.address().port);
+});
+
